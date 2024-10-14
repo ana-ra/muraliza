bc69a33b22a3114c22ad7793b25bc86e164b71a9
Guilherme Ferreira Lenzolari <guilhermeferreiralenzolari@MacBook-Pro-de-Guilherme-2.local>
2024-10-14 10:08:18 -0300

    starts XCode project

---
+++ Muralisa/Muralisa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Muralisa
+//
+//  Created by Guilherme Ferreira Lenzolari on 14/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Muralisa/Muralisa/MuralisaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuralisaApp.swift
+//  Muralisa
+//
+//  Created by Guilherme Ferreira Lenzolari on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MuralisaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
