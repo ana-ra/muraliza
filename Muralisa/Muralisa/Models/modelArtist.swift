594a16992933033c913a5bb382214b83adb5932b
brunooohd <email@blahblah.com>
2024-10-15 11:32:07 -0300

    add artist model

--- Muralisa/Muralisa/Models/modelArtist.swift
+++ Muralisa/Muralisa/Models/modelArtist.swift
@@ -4,4 +4,26 @@
 //
 //  Created by Silvana Rodrigues Alves on 15/10/24.
 //
+import Foundation
+import SwiftUI
+import CloudKit
 
+
+class Artist {
+    static let recordType: String = "Artist"
+    private let id: CKRecord.ID
+    let name: String
+    let image: Image?
+    let works: [Works]
+    let biography: String?
+    
+    
+    init(id: CKRecord.ID, name: String, image: Image?, works: [Works], biography: String?) {
+        self.id = id
+        self.name = name
+        self.image = image
+        self.works = works
+        self.biography = biography
+    }
+    
+}
