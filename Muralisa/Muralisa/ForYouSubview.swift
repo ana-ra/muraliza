33ae1485454936393a6e530cedb5f0c088ab9f12
brunooohd <email@blahblah.com>
2024-10-22 11:31:32 -0300

    Creating ForYou subview

---
+++ Muralisa/Muralisa/ForYouSubview.swift
@@ -0,0 +1,42 @@
+//
+//  ForYouSubview.swift
+//  Muralisa
+//
+//  Created by Bruno Dias on 21/10/24.
+//
+
+import SwiftUI
+
+//@State var pageAtualization: Bool = false
+var mockedList: [String] = ["imagePlaceholder", "imagePlaceholder", "imagePlaceholder","imagePlaceholder", "imagePlaceholder", "imagePlaceholder"]
+
+struct ForYouSubview: View {
+    var body: some View {
+        ScrollView(.horizontal){
+        HStack{
+            ForEach(mockedList, id: \.self) { item in
+                
+                Button {
+                    print("id: \(item)")
+                } label: {
+                    Image(item)
+                        .resizable()
+                        .frame(width: 143, height: 128)
+                }
+
+                    
+                
+                    
+            }.padding(.trailing, -8)
+            }
+        }
+
+
+
+
+    }
+}
+
+#Preview {
+    ForYouSubview()
+}
