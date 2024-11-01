1801488aeae30b20bb39bc4b1609cf6b21cc4a4a
Gustavo Sacramento <gustavo.sacramento211@gmail.com>
2024-11-01 16:29:58 -0300

    Added network monitor to know when there's internet connection. Updated location manager to ask for permission when button is pressed. Created respective empty states.

---
+++ Muralisa/Muralisa/Views/EmptyState/NoConnectionView.swift
@@ -0,0 +1,29 @@
+//
+//  DisabledLocationView.swift
+//  Muralisa
+//
+//  Created by Gustavo Sacramento on 01/11/24.
+//
+
+import SwiftUI
+
+struct NoConnectionView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 24) {
+            // Illustration will be here in the future
+            Image(systemName: "network.slash")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundStyle(.secondary)
+            
+            Text("Você está sem conexão com a internet. Habilite para consumir e colaborar com obras de arte urbana.")
+                .font(.body)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.center)
+        }
+    }
+}
+
+#Preview {
+    NoConnectionView()
+}
