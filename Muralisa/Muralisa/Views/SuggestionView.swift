df0c0dafccdf8dc0eeb7770efb2c32684258c8a1
Gustavo Sacramento <gustavo.sacramento211@gmail.com>
2024-11-02 13:44:45 -0300

    Added route, address and distance location service. Some minor fixes in Curation and Suggestion view.

--- Muralisa/Muralisa/Views/SuggestionView.swift
+++ Muralisa/Muralisa/Views/SuggestionView.swift
@@ -9,15 +9,17 @@ import SwiftUI
 
 struct SuggestionView: View {
     @SceneStorage("isZooming") var isZooming: Bool = false
-    
+    @StateObject var locationManager = LocationManager()
     @StateObject var recommendationService = RecommendationService()
     @StateObject var manager = CachedArtistManager()
     @StateObject var imageService = ImageService()
-    @StateObject var viewModel = SuggestionViewModel()
     @State var isCompressed: Bool = true
     @State var isFetched: Bool = false
     @State var showArtistSheet: Bool = false
     @State var selectedArtist: Artist?
+    @State var address: String = ""
+    @State var distance: Double = -1
+    let locationService = LocationService()
     
     var body: some View {
         NavigationStack {
@@ -27,7 +29,14 @@ struct SuggestionView: View {
                         ImageSubview(work: recommendationService.todayWork, isCompressed: $isCompressed)
                             .zIndex(isZooming ? 1000 : 0)
                         DescriptionSubview(work: recommendationService.todayWork)
-                        ArtistSubview(manager: manager, work: recommendationService.todayWork, address: "Rua Albertina de Jesus Martins, 35", distance: 5000, date: viewModel.distanceDate(from: recommendationService.todayWork.creationDate), showArtistSheet: $showArtistSheet, selectedArtist: $selectedArtist)
+                        ArtistSubview(locationService: locationService,
+                                      locationManager: locationManager,
+                                      manager: manager,
+                                      work: $recommendationService.todayWork,
+                                      address: $address,
+                                      distance: $distance,
+                                      date: distanceDate(from: recommendationService.todayWork.creationDate),
+                                      showArtistSheet: $showArtistSheet, selectedArtist: $selectedArtist)
                         TagsSubView(work: recommendationService.todayWork)
                         
                         VStack(spacing: 24) {
@@ -42,7 +51,7 @@ struct SuggestionView: View {
                 .toolbarBackgroundVisibility(isZooming ? .visible : .automatic, for: .navigationBar)
             } else {
                 // TODO: Design Empty state view or fetching message
-                Text("Fetching your daily works")
+                ProgressView("Fetching your daily works")
             }
         }
         .sheet(isPresented: $showArtistSheet) {
@@ -52,30 +61,11 @@ struct SuggestionView: View {
             }
         }
         .refreshable {
-            Task {
-                do {
-                    try await recommendationService.setupRecommendation2()
-                    try await manager.load(from: recommendationService.todayWork.artist)
-                    withAnimation {
-                        isFetched.toggle()
-                        isFetched.toggle()
-                    }
-                } catch {
-                    print("deu erro \(error.localizedDescription)")
-                }
-            }
+            await fetchData()
             print("refreshed")
         }
         .task {
-            do {
-                try await recommendationService.setupRecommendation2()
-                try await manager.load(from: recommendationService.todayWork.artist)
-                withAnimation {
-                    isFetched = true
-                }
-            } catch {
-                print("deu erro \(error.localizedDescription)")
-            }
+            await fetchData()
         }
     }
 }
